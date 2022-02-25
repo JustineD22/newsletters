677579e472108f72d753248e551ff2fe0d001ffb
Justine <98328323+JustineD22@users.noreply.github.com>
2022-02-25 16:21:43 +0100

    modif home

--- includes/home.inc.php
+++ includes/home.inc.php
@@ -1,3 +1,16 @@
+<?php if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
+    $bienvenue = "<p>";
+    $bienvenue .= "Bonjour ";
+    $bienvenue .= $_SESSION['prenom'];
+    $bienvenue .= " ";
+    $bienvenue .= $_SESSION['nom'];
+    $bienvenue .= "</p>";
+    echo $bienvenue;
+}
+else {
+
+?>
+
 <h1>Bienvenue sur SendNews</h1>
 <p>Pour recevoir des NewsLetters</p>
 <br>
@@ -5,7 +18,7 @@
     <ul>
         <li> 
             <label for="email"> E-mail :</label> 
-            <input type="text" id="email" name="email"  value="Saisir email" />
+            <input type="text" id="email" name="email" />
         </li>
         <li>
             <input type="reset" value="Effacer" />
@@ -18,13 +31,64 @@
 
 <?php
 
+    if (isset($_POST['confirmer'])) {
+        $email = $_POST['email'] ?? '';
+        $erreur = array();
 
-if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
-    $bienvenue = "<p>";
-    $bienvenue .= "Bonjour ";
-    $bienvenue .= $_SESSION['prenom'];
-    $bienvenue .= " ";
-    $bienvenue .= $_SESSION['nom'];
-    $bienvenue .= "</p>";
-    echo $bienvenue;
+
+
+        if (count($erreur) === 0) {
+            $serverName = "localhost";
+            $userName = "root";
+            $database = "sendnews";
+            $userPassword = "";
+
+            try {
+                $conn = new PDO("mysql:host=$serverName;dbname=$database", $userName, $userPassword);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);                
+                $requete = $conn->prepare("SELECT * FROM t_users WHERE USERMAIL='$email'");
+                $requete->execute();
+                $resultat = $requete->fetchAll(PDO::FETCH_OBJ);
+            
+                if(count($resultat) !== 0) {
+                    echo "<p>Votre adresse est déjà enregistrée dans la base de données</p>";
+                }
+
+                else {
+                    $query = $conn->prepare("
+                    INSERT INTO t_users(ID_ROLE, USERMAIL)
+                    VALUES (:id_role, :usermail)
+                    ");
+
+                    $id = 1;
+                    $query->bindParam(':id_role', $id);
+                    $query->bindParam(':usermail', $email, PDO::PARAM_STR_CHAR);
+                    $query->execute();
+                    
+                    echo "<p>Votre adresss E-mail à été envoyé</p>";
+                }
+            } catch (PDOException $e) {
+                die("Erreur :  " . $e->getMessage());
+            }
+
+            $conn = null;
+        } else {
+            $messageErreur = "<ul>";
+            $i = 0;
+            do {
+                $messageErreur .= "<li>" . $erreur[$i] . "</li>";
+                $i++;
+            } while ($i < count($erreur));
+
+            $messageErreur .= "</ul>";
+
+            echo $messageErreur;
+        }
+    } else {
+        echo "<h2>Merci de renseigner le formulaire&nbsp;:</h2>";
+        $name  = $email = '';
+    }
+
 }
+
+
