7bf02b1868426209a7edb270a18629acbbf8e099
Justine <98328323+JustineD22@users.noreply.github.com>
2022-02-25 16:21:02 +0100

    ajout

---
+++ function/getIDsubjects.php
@@ -0,0 +1,19 @@
+<?php
+function getIDsubjects() 
+{
+        $serverName = "localhost";
+        $userName = "root";
+        $database = "sendnews";
+        $userPassword = "";
+        
+        $conn = new PDO("mysql:host=$serverName;dbname=$database", $userName, $userPassword);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $requete = $conn->prepare("SELECT ID_SUBJECT FROM t_subjects ORDER BY SUBCONTENT ASC");
+        $requete -> execute();
+        $resultat = $requete->fetchAll(PDO::FETCH_ASSOC); 
+
+        foreach ($resultat as $key => $value) {
+        }
+        return $resultat;
+}
